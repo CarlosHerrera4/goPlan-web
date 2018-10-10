66046ccef1ab348300826bbcda146e5edb957e04
MundoGister <mundogister@gmail.com>
2018-10-11 01:40:55 +0200

    feat: update event item

--- src/app/components/event/event-item/event-item.component.ts
+++ src/app/components/event/event-item/event-item.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { UserService } from './../../../shared/services/user.service';
+import { SessionService } from './../../../shared/services/session.service';
+import { Component, OnInit, Input, OnDestroy } from '@angular/core';
+import { Router } from '@angular/router';
+import { User } from '../../../shared/models/user.model';
 
 @Component({
   selector: 'app-event-item',
   templateUrl: './event-item.component.html',
   styleUrls: ['./event-item.component.css']
 })
-export class EventItemComponent implements OnInit {
+export class EventItemComponent implements OnInit, OnDestroy {
+  @Input() user: User = new User();
+  authUser: User = new User();
+  onAuthUserChanges: Subscription;
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private SessionService: SessionService,
+    private UserService: UserService
+  ) { }
 
   ngOnInit() {
+    this.authUser = this.sessionService.user;
+    this.onAuthUserChanges = this.sessionService.onUserChanges()
+      .subscribe((user: User) => this.authUser = user);
+  }
+
+  ngOnDestroy() {
+    this.onAuthUserChanges.unsubscribe();
   }
 
 }
