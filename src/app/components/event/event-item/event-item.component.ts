2e5a448afd9f6d43bc8d33ff3d53d75adff734c7
MundoGister <mundogister@gmail.com>
2018-10-11 02:08:26 +0200

    feat: update event item

--- src/app/components/event/event-item/event-item.component.ts
+++ src/app/components/event/event-item/event-item.component.ts
@@ -1,5 +1,7 @@
+import { EventService } from './../../../shared/services/event.service';
 import { Subscription } from 'rxjs';
 import { UserService } from './../../../shared/services/user.service';
+import { Event } from './../../../shared/models/event.model';
 import { SessionService } from './../../../shared/services/session.service';
 import { Component, OnInit, Input, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
@@ -11,20 +13,21 @@ import { User } from '../../../shared/models/user.model';
   styleUrls: ['./event-item.component.css']
 })
 export class EventItemComponent implements OnInit, OnDestroy {
-  @Input() user: User = new User();
-  authUser: User = new User();
+  @Input() event: Event = new Event();
+  authUser: Event = new Event();
   onAuthUserChanges: Subscription;
 
   constructor(
     private router: Router,
-    private SessionService: SessionService,
-    private UserService: UserService
+    private sessionService: SessionService,
+    private userService: UserService,
+    private eventService: EventService
   ) { }
 
   ngOnInit() {
     this.authUser = this.sessionService.user;
     this.onAuthUserChanges = this.sessionService.onUserChanges()
-      .subscribe((user: User) => this.authUser = user);
+      .subscribe((event: Event) => this.authUser = event);
   }
 
   ngOnDestroy() {
