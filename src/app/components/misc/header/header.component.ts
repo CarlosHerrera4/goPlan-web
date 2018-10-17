9e3a89543751b6103a144a3ff4f9ad12b8701849
MundoGister <mundogister@gmail.com>
2018-10-18 00:18:56 +0200

    feat: update header

--- src/app/components/misc/header/header.component.ts
+++ src/app/components/misc/header/header.component.ts
@@ -6,7 +6,8 @@ import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
-  templateUrl: './header.component.html'
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit, OnDestroy {
   user: User;
